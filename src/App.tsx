6f24092cf4ff39b6f6be76850fcbef8eccf55129
Fahim Chowdhury <fahim.arisaftech@gmail.com>
2023-04-07 03:54:06 +0600

    network call added with basic auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-
-import {  Button, MenuProps, Select, Table, Tag } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
+import JsPDF from 'jspdf';
 
 import { Layout, Space } from 'antd';
 import { FormControl, FormHelperText, InputLabel, MenuItem } from '@mui/material';
@@ -33,25 +31,30 @@ const footerStyle: React.CSSProperties = {
   textAlign: 'center',
   color: '#fff',
   backgroundColor: '#7dbcea',
+  height: "150px"
 };
-
 
 
 function App() {
 
-  const [age, setAge] = React.useState('');
-  const [stack, setStack] = useState("flask")
-
-  const handleChange = () => {
-
-  }
+  const [stack, setStack] = useState("django")
 
   const [d, setD] = useState("");
 
   console.log(d);
 
   useEffect(() => {
-    fetch(`http://localhost:8000/apis/v1/${stack}`)
+    fetch(`http://localhost:7000/apis/v1/${stack}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          'Authorization': 'Basic '+btoa(`${process.env.REACT_APP_USERNAME}:${process.env.REACT_APP_PASSWORD}`), 
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        )
+      }
+
+    )
     .then(response => response.json())
     .then(res => setD(JSON.stringify(res)))
   }, []);
@@ -61,7 +64,7 @@ function App() {
       <Space direction="vertical" style={{ width: '100%', height: "100%", gap: "0px 0px" }} size={[0, 48]}>
         <Header style={headerStyle}>AriSaf Tech Ltd.</Header>
         {/* dropdown starts here */}
-        <div className="flex">
+        <div style={{ display: "flex", gap: "50px" }}>
           {/* <FormControl fullWidth>
             <InputLabel id="menu">Age</InputLabel>
             <Select
@@ -77,12 +80,22 @@ function App() {
             </Select>
           </FormControl> */}
 
+          <div>dropdown</div>
+          <div>dropdown</div>
+          <div>dropdown</div>
+          <div>dropdown</div>
+
           
         </div>
         {/* dropdown ends here */}
+
+
+        <div>
+         table
+        </div>
         
         {/* <Content style={contentStyle}>Content</Content> */}
-        <Footer style={footerStyle}>Footer</Footer>
+        <Footer style={footerStyle}>Information about the company</Footer>
       </Space>
       
       {/* <Table columns={columns} dataSource={data} /> */}
